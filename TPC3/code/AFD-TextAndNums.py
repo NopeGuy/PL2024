dfa0b999747e4b027c6ad96146cd307e4e92768a
Luís Ferreira <luisgmsferreira@gmail.com>
2024-02-25 12:57:44 +0000

    corrected 👌

--- TPC3/code/AFD-TextAndNums.py
+++ TPC3/code/AFD-TextAndNums.py
@@ -2,7 +2,7 @@ class FiniteStateAutomaton:
     def __init__(self):
         self.states = {'ON', 'OFF'}
         self.alphabet = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '='}
-        self.transitions = {'ON': {'0': 'ON', '1': 'ON', '2': 'ON', '3': 'ON', '4': 'ON', '5': 'ON', '6': 'ON', '7': 'ON', '8': 'ON', '9': 'ON', '+': 'OFF', '-': 'OFF', '=': 'PRINT'},
+        self.transitions = {'ON': {'0': 'ON', '1': 'ON', '2': 'ON', '3': 'ON', '4': 'ON', '5': 'ON', '6': 'ON', '7': 'ON', '8': 'ON', '9': 'ON', '+': 'OFF', '-': 'OFF'},
                             'OFF': {'0': 'OFF', '1': 'OFF', '2': 'OFF', '3': 'OFF', '4': 'OFF', '5': 'OFF', '6': 'OFF', '7': 'OFF', '8': 'OFF', '9': 'OFF', '+': 'OFF', '-': 'OFF'}}
         self.current_state = 'OFF'
         self.sum_result = 0
@@ -19,20 +19,22 @@ class FiniteStateAutomaton:
         while i < len(input_text):
             three_chars = input_text[i:i+3]
 
-            if 'on' in three_chars:
+            if input_text[i].lower() == 'o' and input_text[i+1].lower() == 'n':
                 i += 1  # Skip two characters after 'on' (one after the loop incrementation and another here)
                 self.current_state = 'ON'
-            elif 'off' in three_chars:
+            elif 'off' in three_chars.lower():
                 self.current_state = 'OFF'
                 i += 2  # Skip three characters after 'off' (same here)
             elif input_text[i] == '=':
                 print("Current sum:", self.sum_result)
             elif input_text[i] in self.alphabet:
                 if self.current_state == 'ON' and input_text[i].isdigit():
-                    numbers = ''.join(c for c in three_chars if c.isdigit())
-                    if numbers:
-                        self.sum_result += int(numbers) 
-                        i += len(numbers) - 1 # Increment counter by number of digits found (minus 1, since the loop will increment it)
+                    while(input_text[i].isdigit()):
+                        self.temp_number += input_text[i]
+                        i += 1
+                    self.sum_result += int(self.temp_number)
+                    self.temp_number = ""
+                    i -= 1  # Decrement counter by 1 to account for the incrementation inside the loop
 
             i += 1
 
@@ -43,7 +45,7 @@ class FiniteStateAutomaton:
 fsa = FiniteStateAutomaton()
 
 # Process input sequences
-input_sequence1 = 'on+123=off456='
+input_sequence1 = '3on+1234=off456='
 result1 = fsa.process_input(input_sequence1)
 print(f"Result for '{input_sequence1}': {result1}")
 
@@ -53,7 +55,7 @@ input_sequence2 = '456on+789ole345=off123='
 result2 = fsa.process_input(input_sequence2)
 print(f"Result for '{input_sequence2}': {result2}")
 
-fsa.reset
+fsa.reset()
 three_chars = ""
 input_sequence3 = open('../input/texto.txt', 'r', encoding='utf-8').read()
 result3 = fsa.process_input(input_sequence3)
