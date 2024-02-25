fcfc26f7dc37200506ef6c8b612c30a2e916a74c
Luís Ferreira <luisgmsferreira@gmail.com>
2024-02-25 01:20:50 +0000

    second one is giving wrong value but rest is gucci

--- TPC3/code/AFD-TextAndNums.py
+++ TPC3/code/AFD-TextAndNums.py
@@ -1,21 +1,21 @@
 class FiniteStateAutomaton:
     def __init__(self):
-        self.states = {'START', 'ON', 'OFF'}
+        self.states = {'ON', 'OFF'}
         self.alphabet = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '='}
-        self.transitions = {'START': {'0': 'ON', '1': 'ON', '2': 'ON', '3': 'ON', '4': 'ON', '5': 'ON', '6': 'ON', '7': 'ON', '8': 'ON', '9': 'ON'},
-                            'ON': {'0': 'ON', '1': 'ON', '2': 'ON', '3': 'ON', '4': 'ON', '5': 'ON', '6': 'ON', '7': 'ON', '8': 'ON', '9': 'ON', '+': 'OFF', '-': 'OFF', '=': 'PRINT'},
+        self.transitions = {'ON': {'0': 'ON', '1': 'ON', '2': 'ON', '3': 'ON', '4': 'ON', '5': 'ON', '6': 'ON', '7': 'ON', '8': 'ON', '9': 'ON', '+': 'OFF', '-': 'OFF', '=': 'PRINT'},
                             'OFF': {'0': 'OFF', '1': 'OFF', '2': 'OFF', '3': 'OFF', '4': 'OFF', '5': 'OFF', '6': 'OFF', '7': 'OFF', '8': 'OFF', '9': 'OFF', '+': 'OFF', '-': 'OFF'}}
-        self.current_state = 'START'
+        self.current_state = 'OFF'
         self.sum_result = 0
         self.temp_number = ""
 
     def reset(self):
-        self.current_state = 'START'
+        self.current_state = 'OFF'
         self.sum_result = 0
         self.temp_number = ""
 
     def process_input(self, input_text):
         i = 0
+
         while i < len(input_text):
             three_chars = input_text[i:i+3]
 
@@ -25,14 +25,17 @@ class FiniteStateAutomaton:
             elif 'off' in three_chars:
                 self.current_state = 'OFF'
                 i += 2  # Skip three characters after 'off' (same here)
-            elif '=' in three_chars:
+            elif input_text[i] == '=':
                 print("Current sum:", self.sum_result)
             elif input_text[i] in self.alphabet:
-                if self.current_state == 'ON':
+                if self.current_state == 'ON' and input_text[i].isdigit():
                     numbers = ''.join(c for c in three_chars if c.isdigit())
-                    self.sum_result += int(numbers)     
-            i += 1       
-            
+                    if numbers:
+                        self.sum_result += int(numbers) 
+                        i += len(numbers) - 1 # Increment counter by number of digits found (minus 1, since the loop will increment it)
+
+            i += 1
+
         return self.sum_result
 
 
